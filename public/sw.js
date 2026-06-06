580f03125b20209ec729e57c1cea347e229d173c
CB GLAMOURS <cbglamours@store.com>
2026-06-06 12:02:30 -0300

    Fix sw.js: remove hardcoded dev asset paths, use runtime caching

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,6 @@
-const CACHE_NAME = 'cb-glamours-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/src/main.js',
-  '/src/styles.css',
-];
+const CACHE_NAME = 'cb-glamours-v2';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
